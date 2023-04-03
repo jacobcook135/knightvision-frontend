3329829901ae0c82959b8499dcc8a5058941845e
jacobcook135 <jacobcook135@gmail.com>
2023-04-03 18:44:37 -0400

    Remove boilerplate code

---
+++ src/views/PlayView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="play">
+    <h1>Play Chess</h1>
+  </div>
+
+
+  <div class="board">
+    <div class="piece">Pawn</div>
+    <div class="piece">Pawn</div>
+    <div class="piece">Pawn</div>
+    <div class="piece">Pawn</div>
+    <div class="piece">Pawn</div>
+    <div class="piece">Pawn</div>
+    <div class="piece">Pawn</div>
+    <div class="piece">Pawn</div>
+  </div>
+</template>
+
+<style>
+.board {
+  grid-column: 1;
+}
+
+@media (min-width: 1024px) {
+  .play {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>

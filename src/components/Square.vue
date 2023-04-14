6c0027647d3aa4723cf0273c905cae05ccb5676b
jacobcook135 <jacobcook135@gmail.com>
2023-04-14 17:25:13 -0400

    Create Square component

---
+++ src/components/Square.vue
@@ -0,0 +1,28 @@
+<template>
+    <button class="square">{{ squareValue }}</button>
+</template>
+ 
+<script lang="js">
+export default {
+    name: "Square",
+    props: {
+        squareValue: String,
+        pos: String,
+    },
+    mounted() {
+        // console.log("from child component squareValue", this.squareValue);
+    }
+}
+</script>
+
+<style scoped>
+.square {
+    background-color: #fff;
+    width: 40px;
+    height: 40px;
+    border: 1px solid #61dafb;
+    text-align: center;
+    font-weight: bold;
+    line-height: 40px;
+}
+</style>
